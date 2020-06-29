b4547e5f5165270d338da23c87a2b184b6267f53
sapna patel <patel.sap@northeastern.edu>
2020-06-29 17:37:29 -0400

    propertifile

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/assignment/target/
-sudo kill -9 `pgrep -f webapp` 
-sudo java -jar assignment-0.0.1-SNAPSHOT.war 
+kill -9 `pgrep -f webapp` 
+chmod 775 assignment-0.0.1-SNAPSHOT.war
+java -jar assignment-0.0.1-SNAPSHOT.war 
