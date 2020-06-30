8727976e0664eaeb178f7b2f011895a1d6ac6439
sapna patel <patel.sap@northeastern.edu>
2020-06-30 11:10:04 -0400

    run

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -4,4 +4,4 @@ source export.sh
 cd /home/ubuntu/assignment/target/
 kill -9 `pgrep -f webapp` 
 chmod 775 assignment-0.0.1-SNAPSHOT.war
-java -jar assignment-0.0.1-SNAPSHOT.war
+Nohup java -jar assignment-0.0.1-SNAPSHOT.war & 
