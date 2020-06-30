bfa1bf34b9fbba0515948064d38edcf5505fba24
sapna patel <patel.sap@northeastern.edu>
2020-06-30 14:14:56 -0400

    fg

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 cd /home/ubuntu/
 source export.sh
 cd /home/ubuntu/assignment/target/
 kill -9 `pgrep -f assignment` 
 sudo chmod 775 assignment-0.0.1-SNAPSHOT.war
-nohup sh -c 'java -jar assignment-0.0.1-SNAPSHOT.war > /dev/null > 2>&1 &' 
+nohup sh -c "java -jar /home/ubuntu/assignment/target/assignment-0.0.1-SNAPSHOT.war > /dev/null 2>&1 &'
