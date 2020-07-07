904fba86b727b045f2517040ef4ed5d8cebd62ef
sapna patel <patel.sap@northeastern.edu>
2020-07-07 18:41:57 -0400

    jsonfile

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -8,3 +8,4 @@ kill -9 `pgrep -f assignment`
 sudo chmod 775 assignment-0.0.1-SNAPSHOT.war
 nohup java -jar /home/ubuntu/assignment/target/assignment-0.0.1-SNAPSHOT.war > /home/ubuntu/webapplog.txt 2> /home/ubuntu/webapplog.txt < /home/ubuntu/webapplog.txt & 
 # nohup sh -c 'java -jar /home/ubuntu/assignment/target/assignment-0.0.1-SNAPSHOT.war > /dev/null 2>&1 &'
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
