06db705b1bb48f4bcad66a67c6eb0d807f6a5b08
sapna patel <patel.sap@northeastern.edu>
2020-07-07 19:12:21 -0400

    jsonfile

--- codedeploy/stop_server.sh
+++ codedeploy/stop_server.sh
@@ -1,4 +1,5 @@
 #!bin/bash
+
 cd /home/ubuntu/
+sudo chown -R ubuntu:ubuntu /home/ubuntu/* 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/home/ubuntu/assignment/src/main/java/com/webapp/assignment/config.json
-# sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
