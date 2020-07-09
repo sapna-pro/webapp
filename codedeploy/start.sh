d7e13d42f911558ac2d48c481b2fa07dfb723bff
sapna patel <patel.sap@northeastern.edu>
2020-07-08 23:02:34 -0400

    services

--- codedeploy/start.sh
+++ codedeploy/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/codedeploy
-cp  your-application.service  /etc/systemd/system/
+sudo chmod 775 your-application.service
+sudo cp  your-application.service  /etc/systemd/system/
 sudo chmod u+x your-script.sh
 sudo systemctl daemon-reload
 sudo systemctl enable your-application.service
